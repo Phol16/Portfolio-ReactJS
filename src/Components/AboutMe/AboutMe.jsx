affd477f9812b259f0d88668836884aec2da91e6
Phol <Pholibertlim@gmail.com>
2022-12-12 20:42:51 +0800

    Fixed: fixed some UI and

--- src/Components/AboutMe/AboutMe.jsx
+++ src/Components/AboutMe/AboutMe.jsx
@@ -5,12 +5,12 @@ import style from './style.module.css'
 const AboutMe = () => {
   return (
     <div className="flex justify-center bg-sky-700 ">
-      <section className="bg-neutral-800 sm:w-4/5 flex flex-col justify-evenly items-center py-20 px-2">
+      <section className="bg-neutral-800 sm:w-4/5 flex flex-col justify-evenly items-center gap-16 py-20 px-2">
         <div className={`${style.topContainer} xl:3/12 lg:w-6/12 sm:w-9/12 leading-8 `}>
           <h1 className="text-sky-500 font-bold mb-3 text-xl 2xl:text-2xl" id='AboutMe'>About Me</h1>
           <p className='sm:text-sm 2xl:text-base indent-5'> My name is Pholibert U. Lim Jr. I spend time learning new things specifically in coding because I'm passionate about learning things or tools that are necessary to improve my knowledge in the tech world. currently I’m knowledgable in using MERN stack. below are more details about me.</p>
         </div>
-        <div className={`${style.bottomContainer} flex sm:flex-row sm:w-9/12 sm:justify-between lg:justify-around`}>
+        <div className={`${style.bottomContainer} flex sm:flex-row sm:w-9/12 sm:justify-between lg:justify-around gap-5`}>
           <section className='mr-1'>
             <h1 className="text-sky-500 font-bold mb-3 sm:text-base 2xl:text-xl">Education</h1>
             <li className='sm:text-sm 2xl:text-base m-1'>Tertiary</li>
@@ -33,7 +33,7 @@ const AboutMe = () => {
               (June 2004 - March 2011)
             </p>
           </section>
-          <section className='flex flex-col justify-around'>
+          <section className='flex flex-col justify-around gap-5'>
             <div>
               <h1 className="text-sky-500 font-bold  mb-3 sm:text-base 2xl:text-xl sm:text-center">Training</h1>
               <li className='sm:text-sm 2xl:text-base'>Uplift CodeCamp</li>
