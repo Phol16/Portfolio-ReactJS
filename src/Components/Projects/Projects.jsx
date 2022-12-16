550a3ef745f62f25750843293efb8b6acf6729e0
Phol <Pholibertlim@gmail.com>
2022-12-16 17:13:14 +0800

    added mazegame site

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -17,7 +17,7 @@ const Projects = () => {
         <h1 className="text-sky-500 font-bold text-xl 2xl:text-2xl mb-10">Projects</h1>
         <section className="flex flex-col justify-center items-center self-center ">
           <ProjectsComponent title={'MovieNotepad'} redirect={'https://movienotepad-clientside.onrender.com/'} image={[movienotepad0, movienotepad1, movienotepad2]} description={'A simple application where you can view all the movies/series and can add it to your watchlist, once added you can view all your listed movies and can add notes to it.'} />
-          <ProjectsComponent title={'TheMazeGame'} redirect={'#'} image={[mazegame0, mazegame1, mazegame2]} description={'A very simple maze game where you can select difficulty from easy to hard and the objective of the game is to make it to the finish line.'} />
+          <ProjectsComponent title={'TheMazeGame'} redirect={'https://phol16.github.io/TheMazeGame/startingPage.html'} image={[mazegame0, mazegame1, mazegame2]} description={'A very simple maze game where you can select difficulty from easy to hard and the objective of the game is to make it to the finish line.'} />
           <ProjectsComponent title={'PokeDex'} redirect={'https://reactjs-pokedex.onrender.com/'} image={[pokedex0, pokedex1, pokedex2]} description={'A very simple application where you can see all of the pokemons by their type and it also has a feature where you can capture them and be able to see all of the captured pokemon on your inventory at MyPokemon button.'} />
         </section>
       </div>
