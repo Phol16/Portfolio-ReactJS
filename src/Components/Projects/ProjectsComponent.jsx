0ee2b9a9a654e682c0e03c318f65fb57df55bded
Phol <Pholibertlim@gmail.com>
2022-12-23 17:53:31 +0800

    fixed some design

--- src/Components/Projects/ProjectsComponent.jsx
+++ src/Components/Projects/ProjectsComponent.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
-const ProjectsComponent = ({ title, description, image, redirect }) => {
+const ProjectsComponent = ({ title, description, image, redirect, tools }) => {
   const navigate = useNavigate();
 
   const intoThe = () => {
@@ -9,11 +9,12 @@ const ProjectsComponent = ({ title, description, image, redirect }) => {
   };
 
   return (
-    <div className="flex flex-col gap-3 my-5 lg:flex-row justify-between items-between ">
+    <div className="flex flex-col gap-3 my-5 lg:flex-row justify-between items-between border rounded-lg p-5 bg-zinc-900">
       <section className="flex flex-col items-center gap-5 ">
         <button className="w-max bg-sky-700 w-44 h-10 text-white hover:bg-sky-900 hover:text-gray-500" onClick={intoThe}>
           <p>{title}</p>
         </button>
+        <p className='lg:w-60 xl:w-80 text-sky-400 text-center'>{tools}</p>
         <p className="lg:w-60 xl:w-80 ">{description}</p>
       </section>
       <section className="flex items-center justify-center gap-1">
